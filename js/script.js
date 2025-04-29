9e9012ebd0067e5c4bb449ed1dc7e53da7ccc23e
brookefiggess <brooke.figgess@mds.ac.nz>
2025-04-29 15:35:50 +1200

    hamburger menu close on click

--- js/script.js
+++ js/script.js
@@ -1,6 +1,13 @@
 const hamburger = document.querySelector('.hamburger');
 const sideMenu = document.getElementById('side-menu');
 
-hamburger.addEventListener('click', () => {
+hamburger.addEventListener('click', (event) => {
+  event.stopPropagation();
   sideMenu.classList.toggle('active');
+});
+
+document.addEventListener('click', (event) => {
+  if (!sideMenu.contains(event.target) && !hamburger.contains(event.target)) {
+    sideMenu.classList.remove('active');
+  }
 });
